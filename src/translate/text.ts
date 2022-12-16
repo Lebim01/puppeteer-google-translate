834f29673ff8d673e49db55942504fb81f2f6e9e
Lebim01 <victoralvarezsaucedo@gmail.com>
2022-12-16 13:06:48 -0600

    Fix get translated text

--- src/translate/text.ts
+++ src/translate/text.ts
@@ -8,26 +8,10 @@ export default async (
 	const { browser, page, timeout } = await launch(opt);
 	try {
 		await page.goto('https://translate.google.com/' + query);
-		const el1 = await page.waitForSelector(
-			'span>span[data-phrase-index="0"]>span[jsaction]',
-			{
-				timeout
-			}
-		);
-		const texts = [await el1.evaluate((e) => e.textContent)];
-		const len = (await page.$$('span>span[data-phrase-index]>span[jsaction]'))
-			.length;
-		if (len === 1) return texts[0];
-		for (let i = 1; i < len; i++) {
-			const el = await page.waitForSelector(
-				`span>span[data-phrase-index="${i}"]>span[jsaction]`,
-				{
-					timeout
-				}
-			);
-			texts.push(await el.evaluate((e) => e.textContent));
-		}
-		return texts.join(' ');
+		const el1 = await page.waitForSelector('span[jsname="txFAF"]>span', {
+		    timeout
+		});
+		return [await el1.evaluate((e) => e.textContent)];
 	} catch (err) {
 		throw err;
 	} finally {
